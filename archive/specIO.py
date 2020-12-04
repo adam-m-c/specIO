45f72ae44c8c47ac05195cfed1dbf0a6c6fff71a
adamchlus <23018310+adamchlus@users.noreply.github.com>
2020-12-04 09:40:23 -0600

    gitignore update

---
+++ archive/specIO.py
@@ -0,0 +1,8 @@
+#load all spectra opening functions
+from openOO import openOO
+from openASD import openASD
+from openSVC import openSVC
+from openSE import openSE
+                                                                                                                                                                                                                                                                                                                  
+                                                        
+                                                                             
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='specIO install',
     version='0.1.2',
-    packages=find_packages(),
+    packages=['specIO'],
     install_requires=[
         'numpy',
         'pandas',
