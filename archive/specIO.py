a2ccf6bcf8f284fcf88a0608b9e56604171fd3a4
adamchlus <23018310+adamchlus@users.noreply.github.com>
2020-12-04 09:41:43 -0600

    gitignore update

--- archive/specIO.py
+++
@@ -1,8 +0,0 @@
-#load all spectra opening functions
-from openOO import openOO
-from openASD import openASD
-from openSVC import openSVC
-from openSE import openSE
-                                                                                                                                                                                                                                                                                                                  
-                                                        
-                                                                             
